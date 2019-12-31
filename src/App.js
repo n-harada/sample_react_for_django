a6e2c084606cdc9238f248542db937a9e64be966
原田眞 <nao0815jas@yahoo.co.jp>
2019-12-31 17:59:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+const API_HOST = "http://127.0.0.1:8000"
 
 function App() {
+
+  const [ goodsList, setGoodsList ] = useState([])
+
+  useEffect(()=>{
+    getGoodsList()
+  },[])
+
+  const renaderGoodsList =()=>{
+    return(
+      goodsList.map(good=><li>{good.name}</li>)
+    )
+  }
+
+  const create=()=>{
+    let data = new FormData()
+    data.append("name", "Name") 
+    axios.post(API_HOST+"/api/good/",
+      data,
+      {headers: {'Content-Type': 'application/x-www-form-urlencoded'}},
+    )
+    .then(response=>{
+      console.log("response", response)
+    })
+    .catch(error=>console.log(error))
+  }
+
+
+  const getGoodsList=()=>{
+    axios.get(API_HOST+"/api/good/")
+    .then(response=>{
+      console.log("response", response)
+      setGoodsList(response.data)
+    })
+    .catch(error=>console.log(error))
+
+  }
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="front_button" onClick={create}>
+          作成
+        </button>
+        <p>グッズ一覧</p>
+        <ul>
+          {renaderGoodsList()}
+        </ul>
       </header>
     </div>
   );
